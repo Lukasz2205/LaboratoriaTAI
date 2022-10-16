d0689e8fb6c7e66b176f6af80b147e9651193027
Lukasz2205 <lukaszgoralczyk2205@gmail.com>
2022-10-16 13:02:08 +0200

    Added api for books

---
+++ app/controllers/books_api_controller.rb
@@ -0,0 +1,45 @@
+class BooksApiController < ApplicationController
+  before_action :set_book, only: %i[ show update destroy ]
+
+  def index
+    @books = Book.all
+
+    render json: @books
+  end
+
+  def show
+    render json: @book
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: { message: 'Successfully created', status: :created, location: @book }
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      render json: { message: 'Successfully updated', data: @book}
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book.destroy
+    render json: { message: "Successfully deleted #{@book.name} with id: #{@book.id}" }
+  end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:name, :description, :pages, :author, :price)
+    end
+end  
